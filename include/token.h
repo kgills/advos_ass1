aeb6f36e554953d3ff582327e372379f3d276ff3
Kevin Gillespie <kgills@gmail.com>
2016-09-13 22:16:44 -0500

    Almost working. Need to add shutdown server.

---
+++ include/token.h
@@ -0,0 +1,10 @@
+#define MAX_PATHLEN 	128
+#define MAX_N 		 	128
+
+typedef struct {
+	unsigned complete; 			// non-zero for completion message
+	unsigned origin; 			// which node the token came from
+	int path_i; 				// current location in the path
+	int path[MAX_PATHLEN];		// path that the token needs to take
+	unsigned sum[MAX_N];		// sum for each node to increment
+} token_t;
--- server/server.c
+++ server/server.c
@@ -14,10 +14,15 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include "token.h"
 
-#define PORT "3490"  // the port users will be connecting to
+#define BACKLOG MAX_N	 // how many pending connections queue will hold
 
-#define BACKLOG 10	 // how many pending connections queue will hold
+#define PORT_IDX 	1
+#define OUT_IDX 	2
+#define N_IDX	 	3
+#define NI_IDX	 	4
+#define ADDR_IDX 	5
 
 void sigchld_handler(int s)
 {
@@ -40,7 +45,60 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(void)
+void client_send(token_t* token, char* hostname, char* port)
+{
+	int sockfd, numbytes;  
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
+
+	printf("client_send %s : %s\n", hostname, port);
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return;
+	}
+
+	// loop through all the results and connect to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			perror("client: connect");
+			close(sockfd);
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return;
+	}
+
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+			s, sizeof s);
+	printf("client: connecting to %s\n", s);
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	// Send the token
+	if (send(sockfd, token, sizeof(token_t), 0) == -1)
+		perror("send");
+
+	close(sockfd);
+}
+
+int main(int argc, char *argv[])
 {
 	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
 	struct addrinfo hints, *servinfo, *p;
@@ -50,13 +108,37 @@ int main(void)
 	int yes=1;
 	char s[INET6_ADDRSTRLEN];
 	int rv;
+	token_t token;
+	int token_sent=0;
+	int i, n, path_len, n_i;
+	unsigned complete[MAX_N];
+	int all_complete;
+
+	if(argc < 6) {
+		fprintf(stderr, "usage: <port> <output_file.txt> <n> <n[i]> <hostname[0]> <port[0]> ... <path_len> <path[0]> ... \n");
+		exit(1);
+	}
+	n = strtol(argv[N_IDX], NULL, 10);
+	n_i = strtol(argv[NI_IDX], NULL, 10);
+	path_len = strtol(argv[NI_IDX+n*2+1], NULL, 10);
+
+	printf("n = %d\n", n);
+	printf("n_i = %d\n", n_i);
+	printf("path_len = %d\n", path_len);
+
+	if(n > MAX_N) {
+		printf("n > MAX_LEN: %d\n", MAX_N);
+	}
+
+	// initialize the complete array
+	memset(complete, 0, sizeof(complete));
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
 
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
@@ -104,11 +186,70 @@ int main(void)
 		exit(1);
 	}
 
+	// Setup the token
+	token.complete = 0;
+	token.origin = n_i;
+	token.path_i = 0;
+	for(i = 0; i < path_len; i++) {
+		token.path[i] = strtol(argv[NI_IDX+n*2+2+i], NULL, 10);
+		token.sum[i] = 0;
+	}
+
+	// Set the last node in the path to this node
+	token.path[i++] = n_i;
+	token.sum[i] = n_i;
+
+	// Finish intializing the token
+	for(;i < MAX_PATHLEN; i++) {
+		token.path[i] = -1;
+		token.sum[i] = 0;
+	}
+
+	printf("path: ");
+	for(i = 0; i < 10; i++) {
+		printf("%d ", token.path[i]);
+	}
+	printf("\n");
+
 	printf("server: waiting for connections...\n");
 
 	while(1) {  // main accept() loop
+
+		// Send the token if this is the first node in the loop
+		if(token_sent == 0) {
+			if(!fork()) { // child process
+
+				// Wait for 10 seconds for all of the servers to come online
+				printf("Waiting for 10s before initiating token\n");
+				sleep(10);
+
+				// Send the initial token
+				printf("Sending initial token\n");
+				client_send(&token, argv[ADDR_IDX+token.path[0]*2], 
+					argv[ADDR_IDX+token.path[0]*2+1]);
+				exit(0);
+			}
+			token_sent = 1;
+		}
+
+		// Test if we have received completion tokens from the rest of the nodes
+		all_complete = 1;
+		for(i = 0; i < n; i++) {
+			if(!complete[i]) {
+				all_complete = 0;
+			}
+		}
+		if(all_complete) {
+
+			// TODO: Print sums for each token to the output file
+
+			printf("Server closing\n");
+			exit(0);
+		}
+
 		sin_size = sizeof their_addr;
 		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+
 		if (new_fd == -1) {
 			perror("accept");
 			continue;
@@ -121,13 +262,51 @@ int main(void)
 
 		if (!fork()) { // this is the child process
 			close(sockfd); // child doesn't need the listener
-			if (send(new_fd, "Hello, world!", 13, 0) == -1)
-				perror("send");
+
+			// Receive the token from the client
+			if (recv(new_fd, &token, sizeof(token_t), 0) == -1)
+				perror("recv");
+
+			printf("Received token from %d\n", token.origin);
 			close(new_fd);
+
+			// Increment the path and sum
+			token.path_i++;
+			token.sum[n_i]++;
+
+			// ***** Parse the token
+			if(token.complete == 1) {
+				complete[token.origin] = 1;
+				printf("Received completion token\n");
+			} else if(token.path[token.path_i] == -1) {
+				// Last token in the path
+				complete[n_i] = 1;
+
+				// Send a completion token to the rest of the nodes
+				token.complete = 1;
+				token.origin = n_i;
+				for(i = 0; i < n; i++) {
+					if(i == n_i) {
+						continue;
+					}
+					client_send(&token, argv[ADDR_IDX+i*2], argv[ADDR_IDX+i*2+1]);
+				}
+				printf("Last token in path\n");
+			} else {
+				// Send it to the next node
+				token.origin = n_i;
+				client_send(&token, argv[ADDR_IDX+token.path[token.path_i]*2], 
+					argv[ADDR_IDX+token.path[token.path_i]*2+1]);
+
+				printf("Token passed along\n");
+			}
+
 			exit(0);
 		}
 		close(new_fd);  // parent doesn't need this
 	}
 
+	printf("Server closing\n");
+	exit(0);
 	return 0;
 }
